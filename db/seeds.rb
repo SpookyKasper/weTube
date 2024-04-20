5f2aaaf75510cb57fa1614a1ca6ea90191ead5c6
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-04-20 14:54:50 +0200

    Add seeds creation of users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+emails = ['jean@pierre.com', 'andy@klein.com', 'peak@time.com', 'kevin@costner', 'mark@iration.com']
+passwords = %w[labonne kleinlein bigparty movieactor dubwise]
+names = %w[jean andy peak kevin mark]
+
+5.times do |i|
+  User.find_or_create_by!(email: emails[i]) do |user|
+    user.password = passwords[i]
+    user.password_confirmation = passwords[i]
+    user.name = names[i]
+  end
+end
