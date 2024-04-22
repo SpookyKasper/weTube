7f38ecace42be9793e96274889fa306387dd2e6d
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-04-22 15:22:43 +0200

    Add info to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,8 @@ class ProfilesController < ApplicationController
   end
 
   def create
-    @profile = Profile.new(profile_params)
+    @user = current_user
+    @profile = @user.build_profile(profile_params)
 
     if @profile.save
       flash[:notice] = 'Your profil information was successfully saved'
@@ -13,6 +14,10 @@ class ProfilesController < ApplicationController
       flash[:alert] = 'There was a problem when saving your profile information'
       render :new, status: 422 # unprocessable_entity
     end
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
   end
 
   private
