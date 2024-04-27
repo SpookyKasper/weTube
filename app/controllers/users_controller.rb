5a5f178bcfc60a2f49d059fe3992b4146526cf86
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-04-27 11:12:25 +0200

    Make header look like youtube header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.includes(:profile).all
     @following = Following.new
     @followees = current_user.followees
+
+    @profile_picture_path = current_user.profile.profile_picture_path || session[:profile_picture_path]
   end
 
   def edit
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   validates :info, presence: true, length: { minimum: 10 }
+  validates :profile_picture_path, presence: true
 end
