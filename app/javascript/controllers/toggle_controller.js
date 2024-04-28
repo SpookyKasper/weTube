84fcf1cdde5fed27a0bcf71573c4cff5b09246a4
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-04-28 19:33:56 +0200

    Implement the display of the dropdown menu with stimulus

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
+  static classes = [ 'show' ]
+  display_menu() {
+    this.element.classList.add(this.showClass)
   }
 }
