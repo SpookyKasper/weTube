65b302b05107b8ee08b2fee8a0f83819cd32ebc2
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-04-29 16:14:17 +0200

    Separate css files

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static classes = [ 'show' ]
   display_menu() {
-    this.element.classList.add(this.showClass)
+    this.element.classList.toggle(this.showClass)
   }
 }
